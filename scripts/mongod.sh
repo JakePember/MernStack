99f04a61bb77635ccc387ee02cd51c5134304de4
Jake <jbles626@gmail.com>
2018-10-27 13:34:02 -0500

    Fixed mongod.sh script to run on my laptop

--- scripts/mongod.sh
+++ scripts/mongod.sh
@@ -1,6 +1,6 @@
 cd "C:\Program Files\MongoDB\Server"
 cd $(ls) #Changes directory to dynamic version folder
-MONGOD="$(PWD)\bin\mongod.exe"
+MONGOD="$(PWD)/bin/mongod.exe"
 
-"MONGOD" #Execute mongod.exe
+"$MONGOD" #Execute mongod.exe
 read -n 1 -s -r -p "Press any key to continue"
